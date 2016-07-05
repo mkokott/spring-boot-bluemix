ac55d074794da50f999a8200dcd5d2fbe4309686
Markus Kokott <markus.kokott@capgemini.com>
2016-07-05 18:19:02 +0200

    working base version

--- src/main/java/org/mkko/trainings/DemoApp.java
+++ src/main/java/org/mkko/trainings/DemoApp.java
@@ -5,7 +5,6 @@ package org.mkko.trainings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 
 /**
--- src/main/java/org/mkko/trainings/controller/DemoController.java
+++ src/main/java/org/mkko/trainings/controller/DemoController.java
@@ -12,10 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DemoController {
-
 	@RequestMapping("/")
 	public String index(){
-		
+
 		return "Greetings from Spring Boot!";
 	}
 }
